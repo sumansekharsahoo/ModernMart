ac95de5fadc2ec1b008f3723c19c27d0c2bef2b7
Suman Sekhar Sahoo <114160074+sumansekharsahoo@users.noreply.github.com>
2023-01-07 00:12:08 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('.csubmit').addEventListener('click',function(){
+    console.log(document.querySelector('#cphno').value);
+
+    if(document.querySelector('#cphno').value==8855 && document.querySelector('#cpwd').value=="msuman"){
+        location.href="customer/chome.html";
+    }
+    else{
+        document.querySelector('.incpass').style.visibility="visible";
+    }
+
+})
