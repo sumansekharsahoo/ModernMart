9687e0c31e8b91b7d2e65f969ce072bbf737a330
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2023-01-08 01:20:08 +0530

    chome update-2

---
+++ customer/chome.js
@@ -0,0 +1,153 @@
+let count1=0;
+document.querySelector('.add1').addEventListener('click',function(){
+    count1++;
+    let qs= Number(document.querySelector('.no1').textContent);
+    document.querySelector('.no1').textContent=count1;
+    console.log(count1);
+})
+document.querySelector('.subt1').addEventListener('click',function(){
+    if(count1>0){
+    count1--;
+    let qs= Number(document.querySelector('.no1').textContent);
+    document.querySelector('.no1').textContent=count1;
+    console.log(count1);
+    }
+})
+
+let count2=0;
+document.querySelector('.add2').addEventListener('click',function(){
+    count2++;
+    let qs= Number(document.querySelector('.no2').textContent);
+    document.querySelector('.no2').textContent=count2;
+    console.log(count2);
+})
+document.querySelector('.subt2').addEventListener('click',function(){
+    if(count2>0){
+    count2--;
+    let qs= Number(document.querySelector('.no2').textContent);
+    document.querySelector('.no2').textContent=count2;
+    console.log(count2);
+    }
+})
+
+
+let count3=0;
+document.querySelector('.add3').addEventListener('click',function(){
+    count3++;
+    let qs= Number(document.querySelector('.no3').textContent);
+    document.querySelector('.no3').textContent=count3;
+    console.log(count3);
+})
+document.querySelector('.subt3').addEventListener('click',function(){
+    if(count3>0){
+    count3--;
+    let qs= Number(document.querySelector('.no3').textContent);
+    document.querySelector('.no3').textContent=count3;
+    console.log(count3);
+    }
+})
+
+
+let count4=0;
+document.querySelector('.add4').addEventListener('click',function(){
+    count4++;
+    let qs= Number(document.querySelector('.no4').textContent);
+    document.querySelector('.no4').textContent=count4;
+    console.log(count4);
+})
+document.querySelector('.subt4').addEventListener('click',function(){
+    if(count4>0){
+    count4--;
+    let qs= Number(document.querySelector('.no4').textContent);
+    document.querySelector('.no4').textContent=count4;
+    console.log(count4);
+    }
+})
+
+
+
+let count5=0;
+document.querySelector('.add5').addEventListener('click',function(){
+    count5++;
+    let qs= Number(document.querySelector('.no5').textContent);
+    document.querySelector('.no5').textContent=count5;
+    console.log(count5);
+})
+document.querySelector('.subt5').addEventListener('click',function(){
+    if(count5>0){
+
+        count5--;
+        let qs= Number(document.querySelector('.no5').textContent);
+        document.querySelector('.no5').textContent=count5;
+        console.log(count5);
+    }
+})
+
+
+let count6=0;
+document.querySelector('.add6').addEventListener('click',function(){
+    count6++;
+    let qs= Number(document.querySelector('.no6').textContent);
+    document.querySelector('.no6').textContent=count6;
+    console.log(count6);
+})
+document.querySelector('.subt6').addEventListener('click',function(){
+    if(count6>0){
+    count6--;
+    let qs= Number(document.querySelector('.no6').textContent);
+    document.querySelector('.no6').textContent=count6;
+    console.log(count6);
+    }
+})
+
+
+let count7=0;
+document.querySelector('.add7').addEventListener('click',function(){
+    count7++;
+    let qs= Number(document.querySelector('.no7').textContent);
+    document.querySelector('.no7').textContent=count7;
+    console.log(count7);
+})
+document.querySelector('.subt7').addEventListener('click',function(){
+    if(count7>0){
+    count7--;
+    let qs= Number(document.querySelector('.no7').textContent);
+    document.querySelector('.no7').textContent=count7;
+    console.log(count7);
+    }
+})
+
+
+let count8=0;
+document.querySelector('.add8').addEventListener('click',function(){
+    count8++;
+    let qs= Number(document.querySelector('.no8').textContent);
+    document.querySelector('.no8').textContent=count8;
+    console.log(count8);
+})
+document.querySelector('.subt8').addEventListener('click',function(){
+    if(count8>0){
+    count8--;
+    let qs= Number(document.querySelector('.no8').textContent);
+    document.querySelector('.no8').textContent=count8;
+    console.log(count8);
+    }
+})
+
+
+let count9=0;
+document.querySelector('.add9').addEventListener('click',function(){
+    count9++;
+    let qs= Number(document.querySelector('.no9').textContent);
+    document.querySelector('.no9').textContent=count9;
+    console.log(count9);
+})
+document.querySelector('.subt9').addEventListener('click',function(){
+    if(count9>0){
+        count9--;
+        let qs= Number(document.querySelector('.no9').textContent);
+        document.querySelector('.no9').textContent=count9;
+        console.log(count9);
+    }
+})
+
