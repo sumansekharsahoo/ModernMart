c3dfb549b80e4e5a4b762bb400283cb454a7de4e
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2023-01-07 20:56:15 +0530

    connections estd

---
+++ customer/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.csubmit').addEventListener('click',function(){
+    console.log(document.querySelector('#cphno').value);
+
+    if(document.querySelector('#cphno').value==600061 && document.querySelector('#cpwd').value=="msuman"){
+        location.href="chome.html";
+    }
+    else{
+        document.querySelector('.incpass').style.visibility="visible";
+    }
+
+})
