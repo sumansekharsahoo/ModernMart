090f537ce9a1e82a3388b522cd8b42bd3b47fb40
Suman Sekhar Sahoo <sumansekharsahoo85@gmail.com>
2023-01-08 11:57:22 +0530

    chome update-3

--- customer/chome.js
+++ customer/chome.js
@@ -3,15 +3,15 @@ document.querySelector('.add1').addEventListener('click',function(){
     count1++;
     let qs= Number(document.querySelector('.no1').textContent);
     document.querySelector('.no1').textContent=count1;
-    console.log(count1);
+    
 })
 document.querySelector('.subt1').addEventListener('click',function(){
     if(count1>0){
     count1--;
     let qs= Number(document.querySelector('.no1').textContent);
     document.querySelector('.no1').textContent=count1;
-    console.log(count1);
     }
+    console.log("hi: "+count1);
 })
 
 let count2=0;
@@ -19,14 +19,15 @@ document.querySelector('.add2').addEventListener('click',function(){
     count2++;
     let qs= Number(document.querySelector('.no2').textContent);
     document.querySelector('.no2').textContent=count2;
-    console.log(count2);
+    
 })
 document.querySelector('.subt2').addEventListener('click',function(){
+
     if(count2>0){
     count2--;
     let qs= Number(document.querySelector('.no2').textContent);
     document.querySelector('.no2').textContent=count2;
-    console.log(count2);
+    
     }
 })
 
@@ -36,14 +37,14 @@ document.querySelector('.add3').addEventListener('click',function(){
     count3++;
     let qs= Number(document.querySelector('.no3').textContent);
     document.querySelector('.no3').textContent=count3;
-    console.log(count3);
+    
 })
 document.querySelector('.subt3').addEventListener('click',function(){
     if(count3>0){
     count3--;
     let qs= Number(document.querySelector('.no3').textContent);
     document.querySelector('.no3').textContent=count3;
-    console.log(count3);
+    
     }
 })
 
@@ -53,14 +54,14 @@ document.querySelector('.add4').addEventListener('click',function(){
     count4++;
     let qs= Number(document.querySelector('.no4').textContent);
     document.querySelector('.no4').textContent=count4;
-    console.log(count4);
+    
 })
 document.querySelector('.subt4').addEventListener('click',function(){
     if(count4>0){
     count4--;
     let qs= Number(document.querySelector('.no4').textContent);
     document.querySelector('.no4').textContent=count4;
-    console.log(count4);
+    
     }
 })
 
@@ -71,7 +72,7 @@ document.querySelector('.add5').addEventListener('click',function(){
     count5++;
     let qs= Number(document.querySelector('.no5').textContent);
     document.querySelector('.no5').textContent=count5;
-    console.log(count5);
+    
 })
 document.querySelector('.subt5').addEventListener('click',function(){
     if(count5>0){
@@ -79,7 +80,7 @@ document.querySelector('.subt5').addEventListener('click',function(){
         count5--;
         let qs= Number(document.querySelector('.no5').textContent);
         document.querySelector('.no5').textContent=count5;
-        console.log(count5);
+        
     }
 })
 
@@ -89,14 +90,14 @@ document.querySelector('.add6').addEventListener('click',function(){
     count6++;
     let qs= Number(document.querySelector('.no6').textContent);
     document.querySelector('.no6').textContent=count6;
-    console.log(count6);
+    
 })
 document.querySelector('.subt6').addEventListener('click',function(){
     if(count6>0){
     count6--;
     let qs= Number(document.querySelector('.no6').textContent);
     document.querySelector('.no6').textContent=count6;
-    console.log(count6);
+    
     }
 })
 
@@ -106,14 +107,14 @@ document.querySelector('.add7').addEventListener('click',function(){
     count7++;
     let qs= Number(document.querySelector('.no7').textContent);
     document.querySelector('.no7').textContent=count7;
-    console.log(count7);
+    
 })
 document.querySelector('.subt7').addEventListener('click',function(){
     if(count7>0){
     count7--;
     let qs= Number(document.querySelector('.no7').textContent);
     document.querySelector('.no7').textContent=count7;
-    console.log(count7);
+    
     }
 })
 
@@ -123,14 +124,14 @@ document.querySelector('.add8').addEventListener('click',function(){
     count8++;
     let qs= Number(document.querySelector('.no8').textContent);
     document.querySelector('.no8').textContent=count8;
-    console.log(count8);
+    
 })
 document.querySelector('.subt8').addEventListener('click',function(){
     if(count8>0){
     count8--;
     let qs= Number(document.querySelector('.no8').textContent);
     document.querySelector('.no8').textContent=count8;
-    console.log(count8);
+    
     }
 })
 
@@ -140,14 +141,84 @@ document.querySelector('.add9').addEventListener('click',function(){
     count9++;
     let qs= Number(document.querySelector('.no9').textContent);
     document.querySelector('.no9').textContent=count9;
-    console.log(count9);
+    
 })
 document.querySelector('.subt9').addEventListener('click',function(){
     if(count9>0){
         count9--;
         let qs= Number(document.querySelector('.no9').textContent);
         document.querySelector('.no9').textContent=count9;
-        console.log(count9);
+        
     }
 })
+
+let tcart=0;
+let flag1=0, flag2=0, flag3=0, flag4=0, flag5=0, flag6=0, flag7=0, flag8=0, flag9=0;
+document.querySelector('.subtc').addEventListener('click', function(){
+    if(count1>0){
+        flag1=1;
+    }
+    else{
+        flag1=0;
+    }
+    
+    if(count2>0){
+        flag2=1;
+    }
+    else{
+        flag2=0;
+    }
+
+
+    if(count3>0){
+        flag3=1;
+    }
+    else{
+        flag3=0;
+    }
+
+    if(count4>0){
+        flag4=1;
+    }
+    else{
+        flag4=0;
+    }
+        // if(count5>0){
+            //     flag5=1;
+            // }
+            // else{
+                //     flag1=0;
+                // }
+                // if(count6>0){
+                    //     flag6=1;
+                    // }
+                    // else{
+                        //     flag1=0;
+                        // }
+                        // if(count7>0){
+                            //     flag7=1;
+                            // }
+                            // else{
+                                //     flag1=0;
+                                // }
+    // if(count8>0){
+    //     flag8=1;
+    // }
+    // else{
+        //     flag1=0;
+        // }
+        // if(count9>0){
+            //     flag9=1;
+            // }
+            // else{
+                //     flag1=0;
+                // }
+    tcart=flag1+flag2+flag3+flag4+flag5+flag6+flag7+flag8+flag9;
+    console.log("hello:" +tcart);
+
+    let qs= Number(document.querySelector('.cartnum').textContent);
+    document.querySelector('.cartnum').textContent=tcart;
+
+})
+
 
