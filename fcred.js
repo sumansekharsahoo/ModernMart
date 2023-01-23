afb23fc4592fd152607e71f06f18b66d1b47163b
Suman Sekhar Sahoo <114160074+sumansekharsahoo@users.noreply.github.com>
2023-01-23 23:43:47 +0530

    Update fcred.js

--- fcred.js
+++ fcred.js
@@ -3,10 +3,10 @@ const date = new Date();
 let day = String(date.getDate());
 let month = String(date.getMonth() + 1);
 let year = String(date.getFullYear());
-if(day.length=1){
+if(day.length===1){
     day="0"+day;
 }
-if(month.length=1){
+if(month.length====1){
     month="0"+month;
 }
 let dstr= day+month+year;
